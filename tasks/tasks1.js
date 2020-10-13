a6d574c2ff24f39972ec8b0a1f6df11dbb9d6a59
EugeneBatr <bavtruskozena@gmail.com>
2020-10-13 21:08:10 +0300

    A

---
+++ tasks/tasks1.js
@@ -0,0 +1,12 @@
+
+const userName = 'Oleg';
+const userName = 'Viktor';
+const userName = 'Mark';
+
+function sayHello() {
+        if (userName = 'Mark') {
+                let message = 'Hi, ' + userName;
+                alert(messege);
+        }
+
+}
