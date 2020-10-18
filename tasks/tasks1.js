6f4b300c29a6598ab0a65800c71eb6840754ae65
IT Shatle <itshatle@iMac-iMac.local>
2020-10-18 15:26:27 +0300

    s

--- tasks/tasks1.js
+++ tasks/tasks1.js
@@ -1,8 +1,23 @@
-const numbers = [1, 2, 35, 64, 58, 60, 7];
-const user = (arr) => {
-        if (!Array.isArray(arr)) return;
-        return arr.map((number) => {
-                return `User ${number}`;
-        });
-}
-console.log(user(numbers));
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// const user = (arr) => {
+//         if (!Array.isArray(arr)) return;
+//         return arr.map((number, index, array) => {
+//                 return index === array.length - 1
+//                         ? `${index} User ${number * 2}`
+//                         : `User ${number}`;
+
+//         });
+
+// }
+// console.log(user(numbers));
+
+
+// четная функция 
+
+// const somevalue = (arr) => {
+//         if (!Array.isArray(arr)) return;
+//         return arr.filter((number) = number % 2 === 0); 
+// };
+// console.log(somevalue(numbers));
+
